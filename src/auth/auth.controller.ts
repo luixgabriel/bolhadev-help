e41556bf38666e86c45a783232a15bb672800211
luix gabriel <luisgasbriel@gmail.com>
2023-08-24 11:48:00 -0300

    feat: add user module in auth module

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,25 +1,32 @@
-import { Controller, Get, Post, Body } from '@nestjs/common';
+import { Controller,Post, Body } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthLoginDTO } from './dto/auth-login.dto';
 import { AuthRegisterDTO } from './dto/auth-register.dto';
+import { AuthForgetDTO } from './dto/auth-forget.dto';
+import { UsersService } from 'src/users/users.service';
+import { AuthResetDTO } from './dto/auth-reset.dto';
 
 
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+  constructor(private readonly authService: AuthService, private readonly usersSevice: UsersService) {}
 
   @Post('login')
-  async login(@Body() data: AuthLoginDTO) {
-    return this.authService.createToken(data);
+    async login(@Body() data: AuthLoginDTO) {
+      return this.authService.createToken(data);
   }
 
   @Post('register')
-  async register(@Body() data: AuthRegisterDTO) {
-    return this.authService.checkToken();
+    async register(@Body() data: AuthRegisterDTO) {
+      return this.usersSevice.create(data)
   }
 
   @Post('forget')
-  async forget(@Body() data: any){
+    async forget(@Body() data: AuthForgetDTO){
+  }
+
+  @Post('reset')
+  async reset(@Body() data: AuthResetDTO){
 
   }
 
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,11 +2,15 @@ import { Module } from "@nestjs/common";
 import { JwtModule } from "@nestjs/jwt";
 import { AuthController } from "./auth.controller";
 import { AuthService } from "./auth.service";
+import { UsersModule } from "src/users/users.module";
+import { PrismaModule } from "src/prisma/prisma.module";
 
 @Module({
     imports: [JwtModule.register({
         secret: '123456789'
-    })],
+    }),
+    UsersModule,
+    PrismaModule],
     controllers: [AuthController],
     exports: [AuthService]
 })
