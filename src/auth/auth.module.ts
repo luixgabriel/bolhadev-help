0ea6cd2369a769a23f5827bd42076281ea5fdf2b
Luis Gabriel <luisgasbriel@gmail.com>
2023-10-25 08:28:34 -0300

    chore: config

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,23 +1,19 @@
 import { Module } from "@nestjs/common";
-import {forwardRef } from '@nestjs/common/utils'
 import { JwtModule } from "@nestjs/jwt";
 import { AuthController } from "./auth.controller";
 import { AuthService } from "./auth.service";
-import { GithubStrategy } from "passport-github2";
 import { PrismaModule } from "src/prisma/prisma.module";
 import { UsersModule } from "../users/users.module";
-import { PassportModule } from "@nestjs/passport";
 
 @Module({
     imports: [JwtModule.register({
         secret: '123456789'
     }),
     PrismaModule, 
-    forwardRef(() => UsersModule),
-    PassportModule
+    UsersModule
 ],
     controllers: [AuthController],
-    providers: [AuthService, GithubStrategy],
+    providers: [AuthService],
     exports:[AuthService]
 })
 
