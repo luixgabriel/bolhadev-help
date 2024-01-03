2b3fdaa297cf0f320d44ad279a87f96fceeb9c56
Luis Gabriel <luis16757@gmail.com>
2024-01-03 13:30:04 -0300

    fix: add permission on cors

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@ import * as cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.enableCors({
-    origin: ["https://bolha-dev-help-frontend-v2-luixgabriel.vercel.app", "http://localhost:3000"],
+    origin: ["https://bolha-dev-help-frontend-v2.vercel.app", "http://localhost:3000"],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     preflightContinue: false,
     optionsSuccessStatus: 204,
